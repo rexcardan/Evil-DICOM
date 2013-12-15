256deff3e6c1ae1ff5e874dc7e18f4fef2e553bb
is <is@hotmail.com>
2013-12-15 13:05:47 +0200

    - Facade project added

---
+++ EvilDICOM.Core/EvilDICOM.Facade/MedicalCase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace EvilDICOM.Facade
+{
+    public class MedicalCase
+    {
+        List<Study> _studiesList = new List<Study>(); 
+        List<Series> _seriesList = new List<Series>(); 
+        Patient _patient = new Patient();
+
+        public MedicalCase()
+        {
+        }
+
+        public MedicalCase(Patient patient)
+        {
+            _patient = patient;
+        }
+    }
+}
