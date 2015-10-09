4dfb177c4c3f937201c433d85cee19a64f84148d
SuneBuur <SuneBuur@users.noreply.github.com>
2015-10-09 14:52:05 +0200

    Bug when using big endian transfer syntax
    When writing big endian lengths the bytes were not reversed.
    
    var pathToDicom = @"...";
    var dicom = DICOMFileReader.Read(pathToDicom);
    var bigEndian = new DICOMWriteSettings
    {
        TransferSyntax = TransferSyntax.EXPLICIT_VR_BIG_ENDIAN
    };
    DICOMFileWriter.Write(pathToDicom, bigEndian, dicom);
    
    Also removed the unused WriteBigEndian stub and switched from Enumerable.Reverse to the more efficient Array.Reverse.

--- EvilDICOM/EvilDICOM/Core/IO/Writing/LengthWriter.cs
+++ EvilDICOM/EvilDICOM/Core/IO/Writing/LengthWriter.cs
@@ -24,38 +24,32 @@ namespace EvilDICOM.Core.IO.Writing
 
         public static void Write(DICOMBinaryWriter dw, VR vr, DICOMWriteSettings settings, int length)
         {
-            var lengthBytes = new byte[0];
-            if (!(settings.TransferSyntax == TransferSyntax.IMPLICIT_VR_LITTLE_ENDIAN))
+            if (settings.TransferSyntax == TransferSyntax.IMPLICIT_VR_LITTLE_ENDIAN)
             {
-                switch (VRDictionary.GetEncodingFromVR(vr))
-                {
-                    case VREncoding.ExplicitLong:
-                        dw.WriteNullBytes(2);
-                        lengthBytes = BitConverter.GetBytes(length);
-                        break;
-                    case VREncoding.ExplicitShort:
-                        lengthBytes = BitConverter.GetBytes((ushort) length);
-                        break;
-                    case VREncoding.Implicit:
-                        lengthBytes = BitConverter.GetBytes(length);
-                        break;
-                }
+                return;
             }
-            else if (settings.TransferSyntax == TransferSyntax.EXPLICIT_VR_BIG_ENDIAN)
-            {
-                lengthBytes = BitConverter.GetBytes(length);
-                lengthBytes.Reverse();
-            }
-            else
-            {
-                //Explicit VR Little Endian
-                lengthBytes = BitConverter.GetBytes(length);
-            }
-            dw.Write(lengthBytes);
-        }
 
-        public static void WriteBigEndian(DICOMBinaryWriter dw, VR vr, int length)
-        {
+            var lengthBytes = new byte[0];
+            switch (VRDictionary.GetEncodingFromVR(vr))
+            {
+                case VREncoding.ExplicitLong:
+                    dw.WriteNullBytes(2);
+                    lengthBytes = BitConverter.GetBytes(length);
+                    break;
+                case VREncoding.ExplicitShort:
+                    lengthBytes = BitConverter.GetBytes((ushort)length);
+                    break;
+                case VREncoding.Implicit:
+                    lengthBytes = BitConverter.GetBytes(length);
+                    break;
+            }
+
+            if (settings.TransferSyntax == TransferSyntax.EXPLICIT_VR_BIG_ENDIAN)
+            {
+                Array.Reverse(lengthBytes);
+            }
+
+            dw.Write(lengthBytes);
         }
 
         public static void WriteBigEndian(DICOMBinaryWriter dw, int length, int numberOfBytes)
@@ -64,12 +58,13 @@ namespace EvilDICOM.Core.IO.Writing
             switch (numberOfBytes)
             {
                 case 2:
-                    lengthBytes = BitConverter.GetBytes((ushort) length).Reverse().ToArray();
+                    lengthBytes = BitConverter.GetBytes((ushort) length);
                     break;
                 case 4:
-                    lengthBytes = BitConverter.GetBytes(length).Reverse().ToArray();
+                    lengthBytes = BitConverter.GetBytes(length);
                     break;
             }
+            Array.Reverse(lengthBytes);
             dw.Write(lengthBytes);
         }
     }
