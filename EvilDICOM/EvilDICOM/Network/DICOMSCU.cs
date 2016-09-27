cb7a80565bc20f984dd0de33b8a2a31a1533bb1d
rexcardan <rcardan@uabmc.edu>
2016-09-27 10:59:52 -0500

    Added ping (CEcho) to DICOMSCU class

--- EvilDICOM/EvilDICOM/Network/DICOMSCU.cs
+++ EvilDICOM/EvilDICOM/Network/DICOMSCU.cs
@@ -71,6 +71,26 @@ namespace EvilDICOM.Network
             return responses;
         }
 
+        /// <summary>
+        /// Sends a CEcho request to the input entity
+        /// </summary>
+        /// <param name="ae">the entity to send the request</param>
+        /// <param name="msTimeout">how long to wait in milliseconds before a timeout</param>
+        /// <returns>true if success, false if failure</returns>
+        public bool Ping(Entity ae, int msTimeout = 3000)
+        {
+            var responseSuccess = false;
+            AutoResetEvent ar = new AutoResetEvent(false);
+            DIMSEService.CEchoResponseReceived += (res, asc) =>
+            {
+                responseSuccess = true;
+                ar.Set();
+            };
+            SendMessage(new CEchoRequest(), ae);
+            ar.WaitOne(msTimeout); //Give it 3 seconds
+            return responseSuccess;
+        }
+
         public CStoreRequest GenerateCStoreRequest(Core.DICOMObject toSend, ushort messageId = 1)
         {
             var sel = toSend.GetSelector();
@@ -114,6 +134,21 @@ namespace EvilDICOM.Network
             this.DIMSEService.CMoveResponseReceived -= cr;
             msgId += 2;
             return resp;
+        }
+
+        /// <summary>
+        /// Emits a CMove operation to an entity which moves an image from the entity to the specified AETitle
+        /// </summary>
+        /// <param name="scp">the provider which will perform the move</param>
+        /// <param name="sopUid">the uid of the image to be moved</param>
+        /// <param name="patientId">the patient id of the image</param>
+        /// <param name="toAETite">the entity title which will receive the image</param>
+        /// <param name="msgId">the message id</param>
+        /// <returns>the move response</returns>
+        public CMoveResponse SendCMoveImage(Entity daemon, string patientId, string sopInstanceUid, string toAETite, ref ushort msgId)
+        {
+            var cfindIod = new CFindImageIOD() { PatientId = patientId, SOPInstanceUID = sopInstanceUid };
+            return SendCMoveImage(daemon, cfindIod, toAETite, ref msgId);
         }
     }
 }
