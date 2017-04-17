7d8e4309c44a10c4e9a5c21dd7325ca26336e975
Rex Cardan <rexcardan@gmail.com>
2017-04-17 07:14:43 -0500

    Migrated to .NET Core

--- EvilDICOM/EvilDICOM/Properties/AssemblyInfo.cs
+++ EvilDICOM/EvilDICOM/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EvilDICOM")]
-[assembly: AssemblyCopyright("Copyright Rex Cardan, Ph.D. ©  2016")]
+[assembly: AssemblyCopyright("Copyright Rex Cardan, Ph.D. ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
