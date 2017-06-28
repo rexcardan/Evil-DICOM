49bd2be40eb33187936a212a8560edf87f8cd4d8
rexcardan <rexcardan@gmail.com>
2017-06-28 14:49:40 -0500

    Relaxed null byte constraint on Preamble Reader so files could still be read if they replace null bytes with something else

--- EvilDICOM/EvilDICOM/Core/IO/Reading/DICOMPreambleReader.cs
+++ EvilDICOM/EvilDICOM/Core/IO/Reading/DICOMPreambleReader.cs
@@ -21,15 +21,14 @@ namespace EvilDICOM.Core.IO.Reading
                 byte[] nullPreamble = dr.Take(128);
                 if (nullPreamble.Any(b => b != 0x00))
                 {
-                    L.Instance.Log("Missing 132 null byte preamble.", LogPriority.WARNING);
-                    dr.StreamPosition -= 128; //rewind
-                    return false;
+                    L.Instance.Log("Missing 128 byte null byte preamble.", LogPriority.WARNING);
                 }
                 //READ D I C M
                 byte[] dcm = dr.Take(4);
                 if (dcm[0] != 'D' || dcm[1] != 'I' || dcm[2] != 'C' || dcm[3] != 'M')
                 {
                     L.Instance.Log("Missing characters D I C M in bits 128-131.", LogPriority.WARNING);
+                    dr.StreamPosition -= 132; //Rewind
                     return false;
                 }
             }
