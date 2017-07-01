686bde92b3d3c0f70a554c5fecceafdcf987f115
rexcardan <rexcardan@gmail.com>
2017-07-01 06:46:29 -0500

    small change to selector

--- EvilDICOM/EvilDICOM/Core/Selection/DICOMSelector.cs
+++ EvilDICOM/EvilDICOM/Core/Selection/DICOMSelector.cs
@@ -18,8 +18,28 @@ namespace EvilDICOM.Core.Selection
             return _dicom;
         }
 
+        /// <summary>
+        /// Forge a new element or replace one if it already exists
+        /// </summary>
+        /// <typeparam name="T">the data type of this element</typeparam>
+        /// <param name="el">the element to create or replace</param>
+        /// <returns>the instance of the inserted element</returns>
         public AbstractElement<T> Forge<T>(AbstractElement<T> el)
         {
+            _dicom.ReplaceOrAdd(el);
+            return el;
+        }
+
+        /// <summary>
+        /// Forge a new element or replace one if it already exists
+        /// </summary>
+        /// <typeparam name="T">the data type of this element</typeparam>
+        /// <param name="el">the element to create or replace</param>
+        /// <param name="data">the data for the element (replaces existing data)</param>
+        /// <returns>the instance of the inserted element</returns>
+        public AbstractElement<T> Forge<T>(AbstractElement<T> el, T data)
+        {
+            el.Data = data;
             _dicom.ReplaceOrAdd(el);
             return el;
         }
