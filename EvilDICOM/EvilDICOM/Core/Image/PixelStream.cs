a073487619906957753c31a59b8653680685e9d6
Rex Cardan <rexcardan@gmail.com>
2017-08-12 15:13:24 -0500

    Added set 16 bit values to pixel stream

--- EvilDICOM/EvilDICOM/Core/Image/PixelStream.cs
+++ EvilDICOM/EvilDICOM/Core/Image/PixelStream.cs
@@ -51,7 +51,7 @@ namespace EvilDICOM.Core.Image
         }
 
         /// <summary>
-        /// Converts the byte array to 32 bit integer array
+        /// Converts the integer pixels into bytes and sets the pixel data
         /// </summary>
         /// <returns></returns>
         public void SetValues32(int[] pixels, bool isDataLittleEndian = true)
@@ -61,6 +61,21 @@ namespace EvilDICOM.Core.Image
             for (int i = 0; i < pixels.Length; i++)
             {
                 var data = BitConverter.GetBytes(pixels[i]);
+                if (isDataLittleEndian != BitConverter.IsLittleEndian) Array.Reverse(data);
+                binWriter.Write(data);
+            }
+        }
+
+        /// <summary>
+        /// Converts the integer pixels into bytes and sets the pixel data
+        /// </summary>
+        public void SetValues16(int[] pixels, bool isDataLittleEndian = true)
+        {
+            var binWriter = new BinaryWriter(this);
+            binWriter.BaseStream.Position = 0;
+            for (int i = 0; i < pixels.Length; i++)
+            {
+                var data = BitConverter.GetBytes((Int16)pixels[i]);
                 if (isDataLittleEndian != BitConverter.IsLittleEndian) Array.Reverse(data);
                 binWriter.Write(data);
             }
