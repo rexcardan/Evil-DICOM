7a6bd663d799946801352387de2186bbdc6c893c
rexcardan <rexcardan@gmail.com>
2018-03-16 14:24:05 -0500

    Added CStorer

---
+++ EvilDICOM/EvilDICOM/Network/SCUOps/CStorer.cs
@@ -0,0 +1,51 @@
+﻿using EvilDICOM.Core;
+using EvilDICOM.Core.Logging;
+using EvilDICOM.Network.DIMSE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvilDICOM.Network.SCUOps
+{
+    public class CStorer
+    {
+        private DICOMSCU _scu;
+        private Entity callingEntity;
+
+        public CStorer(DICOMSCU dICOMSCU, Entity callingEntity)
+        {
+            this._scu = dICOMSCU;
+            this.callingEntity = callingEntity;
+        }
+
+        public EventLogger Logger { get { return _scu.Logger; } }
+
+        /// <summary>
+        /// Emits a CStore operation to an entity which moves an image to the specified entity
+        /// </summary>
+        /// <param name="dcm">the DICOM object to send</param>
+        /// <param name="msgId">the message Id of this message</param>
+        /// <returns>a C-Store response of the operation</returns>
+        public CStoreResponse SendCStore(DICOMObject dcm, ref ushort msgId)
+        {
+            var request = GenerateCStoreRequest(dcm, msgId);
+            return _scu.GetResponse<CStoreResponse, CStoreRequest>(request, callingEntity, ref msgId);
+        }
+
+        private CStoreRequest GenerateCStoreRequest(DICOMObject toSend, ushort messageId = 1)
+        {
+            var sel = toSend.GetSelector();
+            var cStoreReq = new CStoreRequest();
+            cStoreReq.AffectedSOPClassUID = sel.SOPClassUID.Data;
+            cStoreReq.Priority = 1;
+            cStoreReq.Data = toSend;
+            cStoreReq.MessageID = messageId;
+            cStoreReq.AffectedSOPInstanceUID = sel.SOPInstanceUID.Data;
+            cStoreReq.MoveOrigAETitle = _scu.ApplicationEntity.AeTitle;
+            cStoreReq.MoveOrigMessageID = messageId;
+            return cStoreReq;
+        }
+    }
+}
