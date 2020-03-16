ad5a2e86ebfe42c33958239e24f4de8acd13d333
Rex Cardan <rexcardan@gmail.com>
2020-03-16 17:01:50 -0500

    Added DICOM Wrapping ability

---
+++ EvilDICOM/EvilDICOM/Core/Wapping/DICOMObjectWrapper.cs
@@ -0,0 +1,59 @@
+﻿#region
+
+using System.Collections.Generic;
+using System.Linq;
+using EvilDICOM.Core.Element;
+using EvilDICOM.Core.Wapping;
+
+#endregion
+
+namespace EvilDICOM.Core.Wrapping
+{
+    public abstract class DICOMObjectWrapper
+    {
+        public DICOMObject DCM { get; set; }
+
+        public DICOMObjectWrapper()
+        {
+        }
+
+        public DICOMObjectWrapper(DICOMObject dicom)
+        {
+            DCM = dicom;
+        }
+
+        public DICOMData<T> GetValue<T>(Tag tag)
+        {
+            return DCM.TryGetDataValue<T>(tag, null);
+        }
+
+        public void SetValue<T>(Tag tag, T value)
+        {
+            DCM.TrySetDataValue(tag, value);
+        }
+
+        /// <summary>
+        ///     Wraps a DICOM sequence with a type that inherits from DICOMObjectWrapper
+        /// </summary>
+        /// <typeparam name="T">the wrapping class for the DICOM objects within the sequence</typeparam>
+        /// <param name="tag">the DICOM tag of the sequence which contains the objects to wrap</param>
+        /// <returns>a list of wrapped DICOM objects</returns>
+        public SequenceItemWrapper<T> GetWrappedSequence<T>(Tag tag) where T : DICOMObjectWrapper, new()
+        {
+            var seq = DCM.TryGetDataValue<DICOMObject>(tag, null);
+            if (seq != null)
+                return new SequenceItemWrapper<T>(seq);
+            return null;
+        }
+
+        /// <summary>
+        ///     Extracts the DICOMObjects out of a list of DICOMObjectWrappers and sets these as the new values in the sequence
+        /// </summary>
+        /// <param name="tag">the DICOM tag of the sequence where to place the newly extracted objects</param>
+        /// <param name="value">the list of wrapped DICOMObjects to go in the sequence</param>
+        public void SetWrappedSequence<T>(Tag tag, List<T> value) where T : DICOMObjectWrapper
+        {
+            DCM.TrySetDataValue<List<DICOMObject>>(tag, value.Select(dw => dw.DCM).ToList());
+        }
+    }
+}
---
+++ EvilDICOM/EvilDICOM/Core/Wapping/SequenceItemWrapper.cs
@@ -0,0 +1,97 @@
+﻿using EvilDICOM.Core.Element;
+using EvilDICOM.Core.Wrapping;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EvilDICOM.Core.Wrapping
+{
+    public class SequenceItemWrapper<T> : IList<T> where T : DICOMObjectWrapper, new ()
+    {
+        private DICOMData<DICOMObject> _seqItems;
+        List<T> internalList;
+
+
+        public SequenceItemWrapper(DICOMData<DICOMObject> seqItems)
+        {
+            _seqItems = seqItems;
+            internalList = new List<T>();
+
+            foreach (var item in _seqItems.MultipicityValue)
+            {
+                internalList.Add(new T() { DCM = item });
+            }
+        }
+
+
+        public T this[int index]
+        {
+            get => internalList[index];
+            set
+            {
+                internalList[index] = value;
+                _seqItems.MultipicityValue[index] = value.DCM;
+            }
+        }
+
+        public int Count => internalList.Count;
+
+        public bool IsReadOnly => false;
+
+        public void Add(T item)
+        {
+            internalList.Add(item);
+            _seqItems.MultipicityValue.Add(item.DCM);
+        }
+
+        public void Clear()
+        {
+            internalList.Clear();
+            _seqItems.MultipicityValue.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return internalList.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            internalList.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return internalList.GetEnumerator();
+        }
+
+        public int IndexOf(T item)
+        {
+            return internalList.IndexOf(item);
+        }
+
+        public void Insert(int index, T item)
+        {
+            internalList.Insert(index, item);
+            _seqItems.MultipicityValue.Insert(index, item.DCM);
+        }
+
+        public bool Remove(T item)
+        {
+            internalList.Remove(item);
+            return _seqItems.MultipicityValue.Remove(item.DCM);
+        }
+
+        public void RemoveAt(int index)
+        {
+            internalList.RemoveAt(index);
+            _seqItems.MultipicityValue.RemoveAt(index);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return internalList.GetEnumerator();
+        }
+    }
+}
